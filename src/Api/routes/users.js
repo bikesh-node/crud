d638680b720186b14b7f136da9645eb210332fba
Bikesh <bikeshchaudhary1211@gmail.com>
2022-11-27 10:34:01 +0545

    get single user

--- src/Api/routes/users.js
+++ src/Api/routes/users.js
@@ -1,26 +1,28 @@
 import express from "express";
 const router = express.Router();
+import { v4 as uuidv4 } from "uuid";
 
-const users = [
-  {
-    name: "John",
-    age: 20,
-  },
-  {
-    name: "Harry",
-    age: 21,
-  },
-];
+const users = [];
 
 router.get("/", (req, res) => {
-  console.log(users);
   res.send(users);
 });
 
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
-  res.send(user);
+  const _id = uuidv4();
+  const userWithId = { ...user, _id };
+
+  users.push(userWithId);
+  res.send(userWithId);
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user._id === id);
+  if (!foundUser) {
+    res.status(404).send({ success: false, message: "User Not found" });
+  }
+  res.send(foundUser);
+});
 export default router;
